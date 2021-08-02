55211e8de2874e7a81078c81e42fe41f249902d5
TretUliy2 <tretuliy2@gmail.com>
2021-08-02 16:54:59 +0300

    added few models

--- manageSw/manage_sw/models.py
+++ manageSw/manage_sw/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Switch(models.Model):
+    switch_vendor =
+
+
+class Vendor(models.Model):
+    name = models.CharField(200)
+
+
+class
